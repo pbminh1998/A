f60ff1ad4482cf5ea03d76ff5436a1310f9a2510
pbminh1998 <pbminh1998@gamil.com>
2025-02-22 20:47:41 +0700

    add file 3

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 var yesBtn, noBtn, img, question;
 var yesBtnScale = 1, noBtnX = 0, imgY = 0, imgIndex = 0;
 var imgList = ["heart1.gif", "shocked1.gif", "think1.gif", "angry1.gif", "crying1.gif", "hug1.gif"];
-var noList = ["Không", "Suy nghĩ lại đi", "think1.gif", "angry1.gif", "crying1.gif", "heart1.gif"];
+var noList = ["Không", "Suy nghĩ lại đi", "Gặp nhau đi mà", "Không được chọn cấy ni", "Không :)))"];
 
 document.addEventListener("DOMContentLoaded", function (event) {
     
@@ -10,8 +10,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function yesFunc() {
     img = document.getElementById("mainImage");
     noBtn = document.getElementById("no");
+    yesBtn = yesBtn = document.getElementById("yes");
+    question = document.getElementById("question");
+
     img.src = "./"+ imgList[imgList.length - 1];
     noBtn.style.display = "none";
+    yesBtn.style.display = "none";
+    question.innerText = "Nhắn cho anh qua mess nha"
 }
 
 function noFunc() {
@@ -30,4 +35,5 @@ function noFunc() {
     img.style.transform = "translateY("+ imgY +"px)";
     question.style.transform = "translateY("+ imgY +"px)";
     img.src = "./"+ imgList[imgIndex];
+    noBtn.innerText = noList[imgIndex];
 }
